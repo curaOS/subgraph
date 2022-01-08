d3edb95c2ecc247786edfeedf695102a5c2b13b2
achraf elghinoussi <afaithraf@gmail.com>
2022-01-08 10:31:15 +0100

    event handler

---
+++ src/mapping.ts
@@ -0,0 +1,35 @@
+import { near, log } from "@graphprotocol/graph-ts";
+
+export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
+  const actions = receipt.receipt.actions;
+
+  for (let i = 0; i < actions.length; i++) {
+    handleAction(
+      actions[i],
+      receipt.receipt,
+      receipt.block.header,
+      receipt.outcome
+    );
+  }
+}
+
+function handleAction(
+  action: near.ActionValue,
+  receipt: near.ActionReceipt,
+  blockHeader: near.BlockHeader,
+  outcome: near.ExecutionOutcome
+): void {
+  // If the user didn't call a method
+  if (action.kind != near.ActionKind.FUNCTION_CALL) {
+    log.info("Early return: {}", ["Not a function call"]);
+    return;
+  }
+
+  const functionCall = action.toFunctionCall();
+  const methodName = functionCall.methodName.toString();
+
+  if (methodName == "claim_media") {
+  } else {
+    log.info("Unhandled method: {}", [methodName]);
+  }
+}
